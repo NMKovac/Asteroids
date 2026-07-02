7869e99e6b40b7442a646c28ff02cdf6097268c6
Nathan Kovac <s4092307@student.rmit.edu.au>
2026-07-02 20:22:58 +1000

    Created very basic game loop utilising pygame documentation

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+def main():
+    print(f"Starting Asteroids with pygame version {pygame.version.ver}")
+    print(f"Screen Width: {SCREEN_WIDTH}\nScreen Height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("white")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+main()
