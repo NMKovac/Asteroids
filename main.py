f0d7d6963a730762ec9e7138cf0d146d5e53d888
Nathan Kovac <s4092307@student.rmit.edu.au>
2026-07-04 16:15:16 +1000

    added player class, and its parent circle_shape, just visual implementation for now

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from logger import log_state
+from player import Player
+
 def main():
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
     print(f"Screen Width: {SCREEN_WIDTH}\nScreen Height: {SCREEN_HEIGHT}")
@@ -13,7 +15,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("white")
+        screen.fill("black")
+        x_player = SCREEN_WIDTH/2
+        y_player = SCREEN_HEIGHT/2
+        new_player = Player(x_player, y_player, PLAYER_RADIUS)
+        new_player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
