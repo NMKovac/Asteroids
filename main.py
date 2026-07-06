d0555b49a217bc4c3ff32c2565216de97398775c
Nathan Kovac <s4092307@student.rmit.edu.au>
2026-07-06 13:19:21 +1000

    added image only of asteroids to the game

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
@@ -13,7 +15,12 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
+
+    new_field = AsteroidField()
 
     x_player = SCREEN_WIDTH/2
     y_player = SCREEN_HEIGHT/2
